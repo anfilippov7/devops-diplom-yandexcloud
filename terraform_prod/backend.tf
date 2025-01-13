0363ab544a1f7cfcf587581d138c72a4fa30e84c
anfilippov7 <sash.f@rambler.ru>
2025-01-13 10:42:55 +0300

    add_cicd5

--- terraform_prod/backend.tf
+++ terraform_prod/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     endpoint = "storage.yandexcloud.net"
-    bucket = "diplom-state"
+    bucket = "cicd-state"
     region = "ru-central1"
-    key = "diplom-state/terraform.tfstate"
+    key = "cicd-state/terraform.tfstate"
     skip_region_validation = true
     skip_credentials_validation = true
   }
